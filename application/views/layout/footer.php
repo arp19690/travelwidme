b57fde0f78cd5e7b65c1bf56d4a4a2fa885b4524
Arpit Maheshwari <arpit@mrbananaapp.com>
2014-12-23 23:56:42 +0530

    to load Google analytics only when on live server and also added demographics code in the google analytics

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -74,25 +74,32 @@
                     checkNavbarBlinkNotification('<?php echo base_url('user/checkConnectRequestAjax'); ?>', '#nav-connect-requests');
 </script>
 
-<!--Google Analytics START-->
-<script>
-    (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-            (i[r].q = i[r].q || []).push(arguments)
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m)
-    })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+<?php
+    if (USER_IP != '127.0.0.1')
+    {
+        ?>
+        <!--Google Analytics START-->
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
 
-    ga('create', 'UA-51020854-1', 'travelwid.me');
-    ga('send', 'pageview');
-
-</script>
-<!--Google Analytics END-->
+            ga('create', 'UA-51020854-1', 'travelwid.me');
+            ga('require', 'displayfeatures');
+            ga('send', 'pageview');
+        </script>
+        <!--Google Analytics END-->
+        <?php
+    }
+?>
 
 </body>
 </html>
